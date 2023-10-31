b5731bb40a868198a26d8fd1fd44746c638749fa
theborgh <borghiborghi@gmail.com>
2023-10-31 10:26:17 +0100

    delete all bugs/comments on project delete

--- src/components/projectDetails/DeleteProject.tsx
+++ src/components/projectDetails/DeleteProject.tsx
@@ -8,37 +8,41 @@ import {
   DialogTrigger,
   DialogDescription,
 } from "@/components/ui/Dialog";
+import { useRouter } from "next/router";
 
 type DeleteProjectProps = {
   projectId: string;
   projectTitle: string;
   children: ReactNode;
 };
+
 export default function AssignBugToDev({
   projectId,
   projectTitle,
   children,
 }: DeleteProjectProps) {
+  const router = useRouter();
   const [confirmationMessage, setConfirmationMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (
     e: React.FormEvent<HTMLFormElement>
   ): Promise<void> => {
     e.preventDefault();
+    setLoading(true);
 
-    console.log("delete project");
-    if (confirmationMessage === projectTitle) {
-      try {
-        const response = await fetch(`/api/project/delete?id=${projectId}`, {
-          method: "DELETE",
-        });
-        if (!response.ok) {
-          throw new Error("Failed to delete project");
-        }
-        console.log("project deleted");
-      } catch (error) {
-        console.error(error);
+    try {
+      const response = await fetch(`/api/project/delete?id=${projectId}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete project");
       }
+      router.push("/dashboard");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -48,10 +52,13 @@ export default function AssignBugToDev({
       <DialogContent>
         <DialogHeader>
           <DialogTitle>
-            Delete project <em>{projectTitle}</em>? This action is irreversible!
+            Permanently delete project <em>{projectTitle}</em>?
           </DialogTitle>
           <DialogDescription>
-            To confirm, type the project name below and click the delete button.
+            This action will{" "}
+            <span className="text-red-600 font-bold">permanently delete</span>{" "}
+            the project, all project bugs and their comments. To confirm, type
+            the project name below and click &apos;delete&apos;.
           </DialogDescription>
         </DialogHeader>
         <form onSubmit={handleSubmit}>
@@ -62,9 +69,10 @@ export default function AssignBugToDev({
           />
           <button
             type="submit"
-            className="bg-red-600 text-white rounded-md px-3 py-2 mt-3"
+            className="bg-red-600 text-white rounded-md px-3 py-2 mt-3 disabled:opacity-50"
+            disabled={loading || confirmationMessage !== projectTitle}
           >
-            Delete
+            {loading ? "Deleting..." : "Delete"}
           </button>
         </form>
       </DialogContent>
--- src/pages/api/project/delete.ts
+++ src/pages/api/project/delete.ts
@@ -7,15 +7,33 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
   const { id } = req.query as { id: string };
 
   try {
-    const developers = await prisma.project.delete({
+    await prisma.comment.deleteMany({
+      where: {
+        Bug: {
+          project: {
+            id,
+          },
+        },
+      },
+    });
+
+    await prisma.bug.deleteMany({
+      where: {
+        project: {
+          id,
+        },
+      },
+    });
+
+    await prisma.project.delete({
       where: {
         id
     }});
 
-    res.json(developers);
+    res.json({ message: "Project and associated bugs and comments deleted successfully" });
   } catch (e) {
-    console.error("Error fetching projects:", e);
-    res.status(500).send("Error fetching developers");
+    console.error("Error deleting project and associated bugs and comments:", e);
+    res.status(500).send("Error deleting project and associated bugs and comments");
   } finally {
     await prisma.$disconnect();
   }
